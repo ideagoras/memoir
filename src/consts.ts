22bc10c9db6951e6c00bc2030dd86a24451275fb
telltale <ideagorasy@gmail.com>
2026-04-25 23:31:38 +0900

    feat: localize blog to Korean and replace starter content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Memoir';
+export const SITE_DESCRIPTION = '기록하고 연결하는 개발/생각 노트';
